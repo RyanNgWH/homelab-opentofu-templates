6da5d7214730d18596b588b3dad2ed2343ed028b
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2024-06-22 01:08:10 +0800

    Add web preset firewall rules

--- proxmox-firewall-instances.tf
+++ proxmox-firewall-instances.tf
@@ -14,7 +14,7 @@ locals {
         flatten([
           for preset in try(yamldecode(file("configs/firewall/${key}.rules.config.yaml")), { presets = [] }).presets :
           yamldecode(templatefile(
-            "configs/firewall/instance.${preset}.rules.config.tftpl",
+            "configs/firewall/instance.${preset.name}.rules.config.tftpl",
             {
               # Basic rules variables
               lan_airport_privilege_ipset = "${proxmox_virtual_environment_firewall_ipset.datacenter["lan_airport_privilege"].name}"
@@ -25,6 +25,12 @@ locals {
               # Uptime rules variables (to be changed when uptime vm is created)
               uptime_alias = "serene_uptime_kuma"
               uptime_name  = "Uptime Kuma"
+
+              # Web rules variables
+              source_alias          = try(preset.vars.source_alias, "\"\"")
+              source_alias_is_ipset = try(preset.vars.source_alias_is_ipset, "false")
+              source_description    = try(preset.vars.source_description, "")
+              application           = try(preset.vars.application, "")
             }
           ))
         ]),
